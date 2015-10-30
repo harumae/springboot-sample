7560303c1781458096c52bbbadab53f1b981b547
Haruki Maezawa <haruki.maezawa@linecorp.com>
2015-10-30 19:00:01 +0900

    - add auditing to entity.

---
+++ src/main/java/local/mywork/springboot/model/AbstractEntity.java
@@ -0,0 +1,29 @@
+package local.mywork.springboot.model;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.util.Date;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AbstractEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@CreatedDate
+	@Column(name = "created_at")
+	private Date createdAt;
+
+	@LastModifiedDate
+	@Column(name = "updated_at")
+	private Date updatedAt;
+}
--- src/main/java/local/mywork/springboot/model/Customer.java
+++ src/main/java/local/mywork/springboot/model/Customer.java
@@ -6,25 +6,15 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
-@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Customer {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
+@Entity
+public class Customer extends AbstractEntity {
 	@Column(name = "first_name")
 	private String firstName;
+
 	@Column(name = "last_name")
 	private String lastName;
-
-	public Customer(String firstName, String lastName) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-	}
 }
